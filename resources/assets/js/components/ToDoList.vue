f8c45efb1b74d6cca8b2840764e428d0205a9272
Luke Sontrop <luke@bluesaltlabs.com>
2017-03-21 13:42:38 -0700

    Adds todolist vue component. Updates readme.

---
+++ resources/assets/js/components/ToDoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <ul id="todo-list">
+        <li class="todo-list-item" v-for="item in items" v-bind:id="item.id">
+            <select v-bind:value="item.type">
+                <option class="type-task" value="0"><!--purpose="Task"-->&#xf10c;</option>
+                <option class="type-task" value="1"><!--purpose="Task Complete"-->&#xf00d;</option>
+                <option class="type-task" value="2"><!--purpose="Task Migrated"-->&#xf060;</option>
+                <option class="type-task" value="3"><!--purpose="Task Scheduled"-->&#xf061;</option>
+                <option class="type-task" value="4"><!--purpose="Event"-->&#xf10c;</option>
+                <option class="type-task" value="5"><!--purpose="Note"-->-</option>
+            </select>
+            <input type="text" v-bind:value="item.title" />
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+                items: [
+                    {
+                        id: 0,
+                        type: 1,
+                        title: 'item1',
+                        description: 'item 1 is an item'
+                    },
+                    {
+                        id: 1,
+                        type: 2,
+                        title: 'item2',
+                        description: 'item 2 is an item'
+                    },
+                ]
+            };
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <!--<div class="panel-heading">Example Component</div>-->
                 <div class="panel-body">
-                    You are logged in!
+                    <todolist></todolist>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<example></example>
 @endsection
